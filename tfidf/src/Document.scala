9475a971cd04e6e3cf38ed6ef330e5a2b8439523
Raghunandan Ramakrishna Rao <r.raghunandan@gmail.com>
2013-01-24 16:59:27 +0530

    tfidf implemented

--- tfidf/src/Document.scala
+++ tfidf/src/Document.scala
@@ -1,6 +1,6 @@
 
 class Document(tokens: Array[String]) {
-  var map = Map[String, Int]()
+  private var map = Map[String, Int]()
 
   for (token <- tokens) {
     increment(token)
@@ -10,14 +10,20 @@ class Document(tokens: Array[String]) {
     map += token -> map.getOrElse(token, 0)
   }
 
-  private def contains(token: String) {
+  private def contains(token: String):Boolean= {
     map.contains(token)
   }
 
-  def tf(token: String) {
-//    map.get(token)/token.length
+  def tf(token: String): Float = {
+    map.getOrElse(token,0).toFloat/token.length
   }
 
-  //def idf(token:String, )
+  def tfidf(token: String, documents:Seq[Document]):Double ={
+    tf(token) * scala.math.log10(documents.length/documents.count((document: Document) => document.contains(token)))
+  }
+
+  def tfidf(documents:Seq[Document]):Iterable[(String,Double)]={
+      for (token<-map.keys) yield (token,tfidf(token,documents))
+  }
 
 }
