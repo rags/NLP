91685ee02226dda765df4777ae328c05d447f4fa
Raghunandan Ramakrishna Rao <r.raghunandan@gmail.com>
2013-01-24 17:57:43 +0530

    added scala libs

--- tfidf/src/Document.scala
+++ tfidf/src/Document.scala
@@ -10,20 +10,22 @@ class Document(tokens: Array[String]) {
     map += token -> map.getOrElse(token, 0)
   }
 
-  private def contains(token: String):Boolean= {
+  def contains(token: String): Boolean = {
     map.contains(token)
   }
 
   def tf(token: String): Float = {
-    map.getOrElse(token,0).toFloat/token.length
+    map.getOrElse(token, 0).toFloat / token.length
   }
 
-  def tfidf(token: String, documents:Seq[Document]):Double ={
-    tf(token) * scala.math.log10(documents.length/documents.count((document: Document) => document.contains(token)))
+  def tfidf(documents: Seq[Document]): Iterable[(String, Double)] = {
+    for (token <- map.keys) yield (token, tf(token) * idf(token, documents))
   }
 
-  def tfidf(documents:Seq[Document]):Iterable[(String,Double)]={
-      for (token<-map.keys) yield (token,tfidf(token,documents))
-  }
+}
 
+
+def idf(token: String, documents: Seq[Document]): Double = {
+  scala.math.log10(documents.length / documents.count((document: Document) =>
+    document.contains(token)))
 }
