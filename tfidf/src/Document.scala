d5c1f191e455dddd6ebdaf086868a0b1f4874115
Raghunandan Ramakrishna Rao <r.raghunandan@gmail.com>
2013-01-28 12:17:10 +0530

    tfidf generation done for all yelp reviews

--- tfidf/src/Document.scala
+++ tfidf/src/Document.scala
@@ -1,5 +1,11 @@
+import collection.mutable.ListBuffer
+import opennlp.tools.tokenize.{TokenizerModel, TokenizerME}
+import org.json4s.jackson.JsonMethods._
+import org.json4s.StreamInput
+import java.io._
 
-class TagCounter{
+
+class TagCounter {
   private var map = Map[String, Int]()
 
   def increment(token: String) {
@@ -10,11 +16,11 @@ class TagCounter{
     map.contains(token)
   }
 
-  def count(token:String) : Int ={
-    map.getOrElse(token,0)
+  def count(token: String): Int = {
+    map.getOrElse(token, 0)
   }
 
-  def words : Set[String] = {
+  def words: Set[String] = {
     map.keySet
   }
 }
@@ -27,22 +33,79 @@ class Document(tokens: Array[String]) {
     tagCounter.increment(token)
   }
 
-  def contains(token:String): Boolean = tagCounter.contains(token)
+  def contains(token: String): Boolean = tagCounter.contains(token)
 
   def tf(token: String): Float = {
-    tagCounter.count(token).toFloat / token.length
+    tagCounter.count(token).toFloat / tokens.length
   }
 
-  def tfidf(documents: Seq[Document]): Iterable[(String, Double)] = {
-    for (token <- tagCounter.words) yield (token, tf(token) * Document.idf(token, documents))
-  }
+
+  def words = tagCounter.words
 
 }
 
-object Document{
+object Document extends App{
+  Document.tfidf
 
-def idf(token: String, documents: Seq[Document]): Double = {
-  scala.math.log10(documents.length / documents.count((document: Document) =>
-    document.contains(token)))
-}
+  def documents: Seq[(String,Document)] = {
+    val docs = ListBuffer[(String,Document)]()
+    val json: _root_.org.json4s.JValue = parse(new StreamInput(getClass.getResourceAsStream("yelp_reviews_3.txt")))
+    val tzer = new TokenizerME(new TokenizerModel(getClass.getResourceAsStream("en-token.bin")))
+
+    for (restaurant <- json.children) {
+      for (review <- restaurant.children) {
+        val text: String = compact(review \ "text").replaceAll("\\.[\r\n]", " ").replaceAll("[\r\n]", " ").toLowerCase()
+        val sentiment: String = compact(review \ "sentiment")
+        val document: Document = new Document(tzer.tokenize(text))
+        docs += ((sentiment, document))
+      }
+    }
+    docs
+  }
+
+  def hello{
+    println("sgd")
+  }
+  def tfidf{
+    println("Started tfidf collection")
+    val docs = documents
+    println("Collected %s docs",docs.size)
+    val writer = new BufferedWriter(new FileWriter(new File("tfidf.csv")))
+    val idfs: Map[String, Double] = idf(docs)
+    println("Done collecting idfs")
+    var cnt =0
+    for ((sentiment,doc)<-docs){
+      cnt+=1
+      if (cnt%100==0){
+        println("processing doc %s",cnt)
+      }
+      writer.write(sentiment + " ")
+      for (token<-doc.words){
+        writer.write(token)
+        writer.write("=")
+        writer.write((doc.tf(token) * idfs.getOrElse(token, 0d)).toString)
+        writer.write(" ")
+      }
+      writer.write("\n")
+    }
+    writer.flush()
+    writer.close()
+
+  }
+
+  def idf(docs:Seq[(String,Document)]): Map[String,Double] = {
+    val tagCounter = new TagCounter()
+
+    for((_,doc)<-docs){
+      for (token<-doc.words){
+        tagCounter.increment(token)
+      }
+    }
+    var map = Map[String,Double]()
+    for (token<-tagCounter.words){
+      map += token -> scala.math.log10(docs.size.toFloat/ tagCounter.count(token))
+    }
+    map
+  }
+
 }
--- tfidf/test/TdIdfGenerator.scala
+++ tfidf/test/TdIdfGenerator.scala
@@ -1,12 +1,49 @@
+import opennlp.tools.tokenize.{TokenizerModel, TokenizerME, Tokenizer}
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
 import org.scalatest.FunSpec
+import scala.util.control.Breaks._
 
-class TdIdfGenerator extends FunSpec{
-  describe("A Stack") {
+class X extends App{
+  Document.tfidf
+}
+class TdIdfGenerator extends FunSpec {
 
-
+  def tokenizer: Tokenizer = {
+    new TokenizerME(new TokenizerModel(getClass.getResourceAsStream("en-token.bin")))
   }
-  describe("A Stack1") {
+
+  describe("tfidf") {
+    it("should do foo") {
+      val (docCnt,tagCounter) = idf
+
+      println(docCnt)
+      println(tagCounter.words.size)
+      //for (token <- tagCounter.words) {
+       // println(token + " " + tagCounter.count(token).toString)
+      //}
+
+    }
+  }
 
 
+  def idf:(Int,TagCounter)= {
+    var docCnt = 0
+    val tagCounter = new TagCounter()
+    val tzer = tokenizer
+    val json: _root_.org.json4s.JValue = parse(new StreamInput(getClass.getResourceAsStream("yelp_reviews_3.txt")))
+
+    for (restaurant <- json.children) {
+      for (review <- restaurant.children) {
+        docCnt += 1
+
+        val text: String = compact(review \ "text").replaceAll("\\.[\r\n]", " ").replaceAll("[\r\n]", " ").toLowerCase()
+        val tokens: Set[String] = new Document(tzer.tokenize(text)).words
+        for (token <- tokens) {
+          tagCounter.increment(token)
+        }
+      }
+    }
+    (docCnt,tagCounter)
   }
 }
